9744daafaf47698ba406b03b19f33c5d6f08f215
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-10-18 23:26:59 +0530

    18-10-22

---
+++ GFG/String/Count nodes of linked list.cpp
@@ -0,0 +1,25 @@
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution
+{
+public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node *head)
+    {
+
+        // Code here
+        int ans = 0;
+        while (head)
+        {
+            head = head->next;
+            ans++;
+        }
+        return ans;
+    }
+};
