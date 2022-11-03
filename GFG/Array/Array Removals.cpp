8560544fef831956f6b55f764058b0be1f6c961a
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-11-03 07:51:09 +0530

    03-11-22

---
+++ GFG/Array/Array Removals.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int removals(vector<int> &arr, int k)
+    {
+        // Code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int res = INT_MIN;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if ((arr[j] - arr[i]) <= k)
+                {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+        return n - res;
+    }
+};
